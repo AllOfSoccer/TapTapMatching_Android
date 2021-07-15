63de95140807819f888476e03048a9c6f65982f4
조중현 <jjoonghyun.cho@navercorp.com>
2021-07-15 09:52:02 +0900

    viewBinding 테스트

--- app/src/main/java/com/example/taptapmatching/Button1Activity.kt
+++ app/src/main/java/com/example/taptapmatching/Button1Activity.kt
@@ -2,10 +2,17 @@ package com.example.taptapmatching
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import com.example.taptapmatching.databinding.ActivityButton1Binding
 
 class Button1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_button1)
+
+        val binding = ActivityButton1Binding(LayoutInflater)
+        setContentView(binding.root)
+
+        Log.d("UIText", "Button1Activity 진입.")
     }
 }
--- app/src/main/java/com/example/taptapmatching/MainActivity.kt
+++ app/src/main/java/com/example/taptapmatching/MainActivity.kt
@@ -3,27 +3,29 @@ package com.example.taptapmatching
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.Toast
+import com.example.taptapmatching.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val button1: Button = findViewById(R.id.button1)
-        button1.setOnClickListener {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
             Toast.makeText(this, "button1", Toast.LENGTH_SHORT).show()
         }
 
-        val button2: Button = findViewById(R.id.button2)
-        button2.setOnClickListener {
+        binding.button2.setOnClickListener {
             Toast.makeText(this, "button2", Toast.LENGTH_SHORT).show()
         }
 
-        val button3: Button = findViewById(R.id.button3)
-        button3.setOnClickListener {
+        binding.button3.setOnClickListener {
             Toast.makeText(this, "button3", Toast.LENGTH_SHORT).show()
         }
 
