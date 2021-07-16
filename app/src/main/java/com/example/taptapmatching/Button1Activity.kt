b109f45aef5dc994f318fad999f59c90fe20d74c
조중현 <jjoonghyun.cho@navercorp.com>
2021-07-16 21:03:24 +0900

    인텐트에 String, Int 값을 넣어서 다음 액티비티에 전달. 다음 액티비티에서 해당 데이터를 입력받아 버튼의 text로 세팅

--- app/src/main/java/com/example/taptapmatching/Button1Activity.kt
+++ app/src/main/java/com/example/taptapmatching/Button1Activity.kt
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
 import com.example.taptapmatching.databinding.ActivityButton1Binding
 
 class Button1Activity : AppCompatActivity() {
+
+    val binding by lazy { ActivityButton1Binding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityButton1Binding(LayoutInflater)
         setContentView(binding.root)
 
-        Log.d("UIText", "Button1Activity 진입.")
+        binding.to1.text = intent.getStringExtra("from1")
+        binding.to2.text = "${intent.getIntExtra("from2", 0)}"
+
+        Toast.makeText(this, "button1", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/taptapmatching/MainActivity.kt
+++ app/src/main/java/com/example/taptapmatching/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.taptapmatching
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,15 +11,20 @@ import com.example.taptapmatching.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
+        // Intent를 만들고 다음 액티비티로 전환.
+        val intent = Intent(this ,Button1Activity::class.java)
+        intent.putExtra("from1", "Hello Bundle")
+        intent.putExtra("from2", 2021)
+
         binding.button1.setOnClickListener {
-            Toast.makeText(this, "button1", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
         }
 
         binding.button2.setOnClickListener {
