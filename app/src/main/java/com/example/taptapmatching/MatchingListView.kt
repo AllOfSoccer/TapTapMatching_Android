bbdfcb4ced1b19c8c8b5d133fd6302663d4cf548
조중현 <jjoonghyun.cho@navercorp.com>
2021-07-17 15:19:55 +0900

    스몰 켈린더 가로 스크롤 가능하게 추가

--- app/src/main/java/com/example/taptapmatching/MatchingListView.kt
+++ app/src/main/java/com/example/taptapmatching/MatchingListView.kt
@@ -2,7 +2,16 @@ package com.example.taptapmatching
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.taptapmatching.databinding.ActivityMatchingListViewBinding
+import com.example.taptapmatching.databinding.ItemRecyclerBinding
+import com.example.taptapmatching.databinding.SmallcalendarRecyclerBinding
+import java.text.SimpleDateFormat
+
+data class SmallDate(var weakDate: String, var weakDay: String)
 
 class MatchingListView : AppCompatActivity() {
 
@@ -11,5 +20,52 @@ class MatchingListView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val data: MutableList<SmallDate> = loadData()
+        var adapter = MatchingListView.CustomAdapter()
+        adapter.listData = data
+        binding.smallCalendarRecyclerView.adapter = adapter
+        binding.smallCalendarRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+    }
+
+    fun loadData(): MutableList<SmallDate> {
+        val data: MutableList<SmallDate> = mutableListOf() // 컬렉션을 선언
+
+        for (no in 1..100) {
+            val date = System.currentTimeMillis()
+            var smallDate = SmallDate("월", "7")
+            data.add(smallDate)
+        }
+
+        return data
+    }
+
+    class CustomAdapter: RecyclerView.Adapter<Holder>() {
+
+        var listData = mutableListOf<SmallDate>()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+            val binding = SmallcalendarRecyclerBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent, false)
+            return Holder(binding)
+        }
+
+        override fun getItemCount(): Int {
+            return listData.size
+        }
+
+        override fun onBindViewHolder(holder: Holder, position: Int) {
+            val memo = listData.get(position)
+            holder.setMemo(memo)
+        }
+    }
+
+    class Holder(val binding: SmallcalendarRecyclerBinding): RecyclerView.ViewHolder(binding.root) {
+        fun setMemo(smallDate: SmallDate) {
+            binding.weakDate.text = smallDate.weakDate
+            binding.weakDay.text = smallDate.weakDay
+        }
     }
 }
+
