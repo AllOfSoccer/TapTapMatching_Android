7a1031f47fa8b815c16f1ee49b8afa250d354327
조중현 <jjoonghyun.cho@navercorp.com>
2021-08-02 20:44:58 +0900

    배열로 필터 리스트 처리

--- app/src/main/java/com/example/taptapmatching/matchingMain/MatchingFilterRecycler.kt
+++ app/src/main/java/com/example/taptapmatching/matchingMain/MatchingFilterRecycler.kt
@@ -7,11 +7,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.taptapmatching.databinding.FragmentSmallFilteringBinding
 
 class MatchingFilterRecycler {
+
+    val filterList: Array<String> = arrayOf("장소", "시간대", "경기", "참가", "실력", "테스트")
+
     fun loadData(): MutableList<FilterInfo> {
         val result: MutableList<FilterInfo> = mutableListOf() // 컬렉션을 선언
 
-        for (no in 1..100) {
-            val title = "장소 ${no}"
+        for (title in filterList) {
             var memo = FilterInfo(title, false)
             result.add(memo)
         }
@@ -23,6 +25,7 @@ class MatchingFilterRecycler {
 
         var listData = mutableListOf<FilterInfo>()
 
+        // 한 화면에 그려지는 아이템 개수만큼 레이아웃 생
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
             val binding = FragmentSmallFilteringBinding.inflate(
                 LayoutInflater.from(parent.context),
@@ -30,11 +33,12 @@ class MatchingFilterRecycler {
             return Holder(binding)
         }
 
+        // 목록의 총 데이터의 개수
         override fun getItemCount(): Int {
             return listData.size
         }
 
-        //생성된 뷰 홀더를 화면에 보여줌
+        // 생성된 뷰 홀더를 화면에 보여줌
         override fun onBindViewHolder(holder: Holder, position: Int) {
             val memo = listData.get(position)
             holder.setMemo(memo)
@@ -44,9 +48,16 @@ class MatchingFilterRecycler {
     class Holder(val binding: FragmentSmallFilteringBinding): RecyclerView.ViewHolder(binding.root) {
         // binding을 생성 시점에 전달받아 해당 바인딩에 있는 데이터를 세팅
         fun setMemo(filterInfo: FilterInfo) {
+
+            binding.textView5.setOnClickListener {
+                val pos = getAdapterPosition()
+                Log.d("ddd", "필터종류 선택됨. ${pos}")
+            }
+
             binding.textView5.text = "${filterInfo.title}"
             Log.d("dd", "${filterInfo.title}")
         }
+
     }
 
     data class FilterInfo(var title: String, var isSelected: Boolean)
