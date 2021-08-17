7d9b3f65bc345f0baba95f426b420016c636da57
조중현 <jjoonghyun.cho@navercorp.com>
2021-08-17 23:45:37 +0900

    네이버지도 sdk 추가

--- app/src/main/java/com/example/taptapmatching/MakeMatchingRoom/MakeTeamMatchingRoomActivity.kt
+++ app/src/main/java/com/example/taptapmatching/MakeMatchingRoom/MakeTeamMatchingRoomActivity.kt
@@ -2,8 +2,11 @@ package com.example.taptapmatching.MakeMatchingRoom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.taptapmatching.databinding.ActivityMakeTeamMatchingRoomBinding
 import com.example.taptapmatching.databinding.ActivityMatchingDetailBinding
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
 
 class MakeTeamMatchingRoomActivity : AppCompatActivity() {
 
